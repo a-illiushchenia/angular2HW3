74e07aa470fe54c7f45b92c8b4d542b8a51dbe86
Aliaksandr <Aliaksandr>
2017-03-05 12:11:43 +0300

    execute home work 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import 'whatwg-fetch';
+import {Subject, Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  public findCount: string;
+  public findList: Array<any>;
+  public subject: Subject<any> = new Subject();
+
+  constructor(){
+    this.subject.debounceTime(300)
+      .switchMap(
+        (searchValue: string) =>
+          Observable.fromPromise(fetch(`https://api.github.com/search/repositories?q=${searchValue}`).then(res => res.json()))
+      ).subscribe(res => {console.log(res); this.findRepositories(res.total_count, res.items)});
+  }
+
+ public onInputToFilter(value: string) {
+   this.subject.next(value);
+ }
+
+  public findRepositories(findCount: string, items: Array<any>){
+    this.findCount = findCount;
+    this.findList = items;
+  }
 }
